53fde8bade39b59e684773da254fdc324e2fbad6
Sahil-max <ksahilreddy@gmail,com>
2022-01-21 23:08:31 -0500

    add files

--- assignment_1.py
+++ assignment_1.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     spark.sparkContext.setLogLevel("ERROR")
     current_dir = os.path.abspath(os.path.dirname(__file__))
 
-    df1 = spark.sparkContext.parallelize([1, 2, 3, 4, 5]).toDF(["c1"])
+    df1 = spark.sparkContext.parallelize([1, 2, 3, 4, 5]).toDF("c1")
     df1.show()
 
 # spark-submit assignment_1.py
